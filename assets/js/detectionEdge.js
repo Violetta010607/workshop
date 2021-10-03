05cb3cc10986d8b80c347d90014f19ba93e2ec2a
Violetta010607 <beatrice.vincenzi@vinet.it>
2021-10-03 11:57:36 +0200

    update application
    update detectionPose and detectionEdge. audible feedback improvements, and how to start the application

--- assets/js/detectionEdge.js
+++ assets/js/detectionEdge.js
@@ -1,10 +1,7 @@
 
 let video;
 var canvas;
-var div;
 
-// let widthCanv = 720;
-// let heightCanv = 480;
 let widthCanv = 320;
 let heightCanv = 256;
 
@@ -13,9 +10,8 @@ let startButton;
 var videoAvailable = false;
 var isStarted = false;
 
-// var detector;
+var detector;
 var posit;
-var pose;
 var markers;
 var currentImage;
 var ismarked = false;
@@ -23,17 +19,21 @@ var ismarked = false;
 var rectWitdh = 0;
 var centrex = 0;
 var centrey = 0;
+var tempx = 0;
 
 var context;
-var tempz = 0;
+
+var sound, soundA, soundCamera;
 
 
 function preload() {
   soundFormats('mp3');
-  soundA = loadSound('assets/js/sounds/maracasTrim.mp3');
+  // sound = loadSound('assets/js/sounds/a.mp3');
   soundCamera = loadSound('assets/js/sounds/cameraOn.mp3');
+  soundA = loadSound('assets/js/sounds/maracasTrim.mp3');
   soundA.setVolume(1.0);
   soundA.rate(1.0);
+  // soundA.playMode('untilDone');
   // soundA.setLoop(true);
 }
 //ARUCO_MIP_36h12
@@ -46,16 +46,17 @@ function videoReady() {
     maxHammingDistance: 6
   });
   console.log(detector);
-  posit = new POS.Posit(40, width);
-  console.log(posit);
   isStarted = true;
 }
 
 
 function setup() {
-
+  // mimics the autoplay policy
+  // getAudioContext().suspend();
   userStartAudio();
-
+  // if(getAudioContext().state === "suspended"){
+  //   getAudioContext().resume();
+  // }
   console.log("setup: "+getAudioContext().state);
 
   // if(windowHeight > windowWidth){
@@ -65,13 +66,11 @@ function setup() {
   //   widthCanv = windowHeight;
   //   heightCanv = windowHeight * 1.5;
   // }
-  console.log("canvas width: "+widthCanv);
-  console.log("canvas height: "+heightCanv);
+  // console.log("canvas width: "+widthCanv);
+  // console.log("canvas height: "+heightCanv);
   // canvas = createCanvas((windowWidth/3)*2, (windowHeight/3)*2);
   canvas = createCanvas(widthCanv, heightCanv);
   canvas.parent("canvas");
-
-  // div = select("#canvas");
 
   background(255);
   video = createCapture(VIDEO, videoReady);
@@ -92,31 +91,26 @@ function setup() {
 // function startVideo(){
 //   if(getAudioContext().state !== "running"){
 //     getAudioContext().resume();
-//     // console.log("startButtonPressed: "+getAudioContext().state);
+//     console.log("startButtonPressed: "+getAudioContext().state);
 //   }
-//
+//   // userStartAudio();
 //   isStarted = true;
-//   div.attribute("aria-disabled", "false");
 // }
 //
 // function pauseVideo(){
 //   if(getAudioContext().state !== "suspended"){
 //     getAudioContext().suspend();
-//     // console.log("stopButtonPressed: "+getAudioContext().state);
+//     console.log("stopButtonPressed: "+getAudioContext().state);
 //   }
 //   isStarted = false;
-//
-//   if(soundA.isPlaying()){
-//     soundA.stop();
-//   }
-//   div.attribute("aria-disabled", "true");
 // }
+
 
 function drawCorners(markers){
   var corners, corner, i, j;
 
   for (i = 0; i !== markers.length; ++ i){
-    // console.log(markers[i]);
+    console.log(markers[i]);
     corners = markers[i].corners;
 
     stroke(0, 255, 0);
@@ -162,41 +156,52 @@ function drawId(markers){
 
     centrex = (xMin + xMax)/2;
     centrey = (yMin + yMax)/2;
-    // console.log(centrex+" "+centrey);
+    console.log("centrex, centrey: "+centrex+", "+centrey);
     strokeWeight(1);
     point(centrex, centrey);
-
+    changeSound(centrex);
     text(markers[i].id, xMin, yMin);
   }
 }
 
-function changeVolume(pose){
-
-  // console.log("pose: "+pose);
-
-  var z = pose.bestTranslation[2];
-  console.log("z: "+z);
-  if(Math.abs(z-tempz) > 5){
-    var newVolume = norm(z, 100, 1000);
-    //// var newVolume = map(z, 100, 2000, 1, 0);
-    newVolume = 1 - (Math.round(newVolume * 10) / 10)
-    // var playbackRate = map(z, 100, 1000, 2, 0);
+function changeSound(centrex){
+  if(Math.abs(centrex-tempx) > 5){
+    let halfWidth = width/2;
+    if(centrex > halfWidth){
+      playbackRate = Math.sqrt((centrex-halfWidth)/halfWidth) + 1;
+      console.log("rate>360: "+playbackRate);
+      //Math.cbrt
+    }else{
+      playbackRate = Math.sqrt(-((centrex-halfWidth)/halfWidth)) + 1;
+      console.log("rate<=360: "+playbackRate);
+    }
+    // var playbackRate = map(centrex, , 2000, 2, 0);
     // playbackRate = constrain(playbackRate, 0.01, 4);
-
-    console.log(newVolume);
-    // console.log(playbackRate);
-
-    soundA.setVolume(newVolume);
-    // soundA.rate(playbackRate);
+    console.log("playbackRate: "+playbackRate);
+    soundA.rate(playbackRate);
     if(!soundA.isPlaying()){
       soundA.play();
     }
-
-    tempz = z;
+    tempx = centrex;
   }
 
+  // var newVolume = norm(centrex, 0, width);
+  // newVolume = Math.round(newVolume * 10) / 10
+  // console.log(newVolume);
+  // soundA.setVolume(newVolume);
+  // stroke(255, 255 ,255);
+  // textSize(20)
+  // text(soundA.getVolume(), 10, height+25);
 }
 
+
+// function detection(currentImage){
+//   markers = detector.detectImage(320, 240, currentImage.pixels);
+//   console.log(markers);
+//   if(markers.length > 0){
+//     ismarked = true;
+//   }
+// }
 
 function draw() {
   // console.log("draw: "+getAudioContext().state);
@@ -209,35 +214,23 @@ function draw() {
 
     currentImage = video.get();
     currentImage.loadPixels();
-    // console.log(currentImage);
+    console.log(currentImage);
     image(currentImage, 0, 0);
     markers = detector.detectImage(width, height, currentImage.pixels);
-    // console.log(markers);
+    console.log(markers);
     if(markers.length > 0){
-      //I am interested only in markers with ID == 2, so I filter by ID
-      for (i = 0; i !== markers.length; ++ i){
-        var markerId = markers[i].id;
-        if(markerId === 2){
-          ismarked = true;
-          pose = posit.pose(markers[i].corners);
-        }
-      }
+      ismarked = true;
     }
+
     if(ismarked){
       drawCorners(markers);
       drawId(markers);
-      changeVolume(pose);
-      // if(!soundA.isPlaying()){
-      //   soundA.play();
-      // }
+      // changeVolume();
+      if(!soundA.isPlaying()){
+        soundA.play();
+      }
       ismarked = false;
     }
-    // else{
-    //   if(soundA.isPlaying()){
-    //     console.log("here");
-    //     soundA.stop();
-    //   }
-    // }
   }else{
     background(255);
   }
